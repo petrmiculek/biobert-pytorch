eea34488008da9e96991d7f755fb03f87eba6084
petrmiculek <petr.miculek@gmail.com>
2021-12-12 22:32:15 +0100

    run-script: fix iteration

--- named-entity-recognition/eval_all.sh
+++ named-entity-recognition/eval_all.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 DIR=../datasets/NER
-DATASETS=$(ls -1 "$DIR")
+#DATASETS=($(ls "$DIR"))
+mapfile -t DATASETS < <(ls "$DIR")
+MODELS=("dmis-lab/biobert-base-cased-v1.1")
+# "dmis-lab/biobert-base-cased-v1.2"
+# "dmis-lab/biobert-v1.1")
 
-for ds in $DATASETS; do
-  s="$ds"
-  export ENTITY="$s"
-  bash train.sh
+# alternative models:
+# dmis-lab/biobert-base-cased-v1.2
+# dmis-lab/biobert-v1.1
+# dmis-lab/bern2-ner
+
+for model in "${MODELS[@]}"; do
+  echo "+++Model: $model"
+  export NER_MODEL="$model"
+      for ds in "${DATASETS[@]}"; do
+        echo "+Dataset: $ds"
+        export ENTITY="$ds"
+        bash train.sh
+      done
 done
--- named-entity-recognition/train.sh
+++ named-entity-recognition/train.sh
@@ -10,22 +10,17 @@ export SAVE_STEPS=1000
 #export ENTITY=NCBI-disease  # passed from outside as a env variable
 export SEED=1
 
-# alternative models:
-# dmis-lab/biobert-base-cased-v1.2
-# dmis-lab/biobert-v1.1
-# dmis-lab/bern2-ner
-
 python run_ner.py \
     --data_dir ${DATA_DIR}/"${ENTITY}"/ \
     --labels ${DATA_DIR}/"${ENTITY}"/labels.txt \
-    --model_name_or_path dmis-lab/biobert-base-cased-v1.1 \
+    --model_name_or_path "${MODEL}" \
     --output_dir ${SAVE_DIR}/"${ENTITY}" \
     --max_seq_length ${MAX_LENGTH} \
     --num_train_epochs ${NUM_EPOCHS} \
     --per_device_train_batch_size ${BATCH_SIZE} \
     --save_steps ${SAVE_STEPS} \
     --seed ${SEED} \
+    --do_train \
     --do_eval \
     --do_predict \
     --overwrite_output_dir
-#    --do_train
